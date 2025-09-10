853b1b3af99359c507e7177580790fb68f85b916
batuhan.baran <batuhan.baran@gelirler.gov.tr>
2025-09-10 13:36:24 +0300

    Initial Commit

---
+++ SupernovaX/SupernovaXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupernovaXApp.swift
+//  SupernovaX
+//
+//  Created by Batuhan Baran on 10.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct SupernovaXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
